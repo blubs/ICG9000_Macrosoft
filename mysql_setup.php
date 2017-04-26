6674b47f7e61e1eae26ffffed2599429f60c3d8e
TheGreed <akhterovd@gmail.com>
2017-04-25 18:03:30 -0700

    Changed mysql_setup to check if script runs correctly

--- mysql_setup.php
+++ mysql_setup.php
@@ -1,11 +1,11 @@
 <?php
 	require 'db.php';
-	$con->query('DROP TABLE IF EXISTS csc');
-	$con->query('DROP TABLE IF EXISTS professors');
-	$con->query('DROP TABLE IF EXISTS users');
-	$con->query('DROP TABLE IF EXISTS user_session');
+	$result = $con->query('DROP TABLE IF EXISTS csc');
+	$result = $con->query('DROP TABLE IF EXISTS professors');
+	$result = $con->query('DROP TABLE IF EXISTS users');
+	$result = $con->query('DROP TABLE IF EXISTS user_session');
 
-	$con->query('CREATE TABLE professors (Faculty varchar(50) PRIMARY KEY NOT NULL, office_hours varchar(120), phone varchar(20), email varchar(80), room varchar(20))');
-	$con->query('CREATE TABLE users (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, username varchar(50) NOT NULL UNIQUE, password varchar(100) NOT NULL, hash varchar(100) NOT NULL, permissions INT DEFAULT 0)');
-	$con->query('CREATE TABLE user_session (id INT NOT NULL, session varchar(100) NOT NULL, FOREIGN KEY (id) REFERENCES users(id))');
+	/* $con->query('CREATE TABLE professors (Faculty varchar(50) PRIMARY KEY NOT NULL, office_hours varchar(120), phone varchar(20), email varchar(80), room varchar(20))'); */
+	/* $con->query('CREATE TABLE users (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, username varchar(50) NOT NULL UNIQUE, password varchar(100) NOT NULL, hash varchar(100) NOT NULL, permissions INT DEFAULT 0)'); */
+	/* $con->query('CREATE TABLE user_session (id INT NOT NULL, session varchar(100) NOT NULL, FOREIGN KEY (id) REFERENCES users(id))'); */
 ?>
