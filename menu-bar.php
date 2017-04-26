b64561a554b2c56d82f903424046d8a2708e3ad7
TheGreed <akhterovd@gmail.com>
2017-04-25 18:13:29 -0700

    Testing if account-management tab is viewable

--- menu-bar.php
+++ menu-bar.php
@@ -6,15 +6,16 @@
 	<div class='menu-bar-item'><a href='edit.php'>Edit</a></div>
 	<div class='menu-bar-item'><a href='print-information-cards.php'>Print</a></div>
 	<?php
-		$id = $_SESSION['id'];
-		$query = "SELECT permissions FROM users WHERE id='$id'";
-		$result = $con->query($query);
-		$row = $result->fetch_assoc();
-		$permissions = $row['permissions'];
+		/* $id = $_SESSION['id']; */
+		/* $query = "SELECT permissions FROM users WHERE id='$id'"; */
+		/* $result = $con->query($query); */
+		/* $row = $result->fetch_assoc(); */
+		/* $permissions = $row['permissions']; */
 
-		if($permissions == 0){
-			echo "<div class='menu-bar-item'><a href='account-management.php'>Account Management</a></div>";
-		}
+		/* if($permissions == 0){ */
+		/* 	echo "<div class='menu-bar-item'><a href='account-management.php'>Account Management</a></div>"; */
+		/* } */
 	?>
+		<div class='menu-bar-item'><a href='account-management.php'>Account Management</a></div>
 	<div id='logout' class='menu-bar-item'><a href='logout.php'>Logout</a></div>
 </div>
