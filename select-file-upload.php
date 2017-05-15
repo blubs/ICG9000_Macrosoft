a452a9e5786859ff65166dd1c221bd7c1ab2315d
blubs <lgutierrez4435@hotmail.com>
2017-05-14 21:22:23 -0700

    -fixed chrome related formatting issues

--- select-file-upload.php
+++ select-file-upload.php
@@ -6,7 +6,6 @@
 		header('location: index.php');
 	}
 ?>
-<!DOCTYPE html>
 <html>
 	<head>
 		<title>Card Generator</title>
@@ -14,7 +13,7 @@
 	</head>
 	<body>
 		<div id='container'>
-		<?php require_once 'menu-bar.php'; ?>
+		<?php include_once('menu-bar.php');?>
 		<div id='center-container'>
 				<h1 class='main-menu-title'>Select File</h1>
 				<form action='insert.php' method='POST' enctype='multipart/form-data'>
